9cf51efe8444073c75fe35828d6e3fe164828ddd
Brett Jones <hello@brettjones.me>
2017-06-18 18:34:16 +0800

    Adding blog post for june

--- lib/reading_time.rb
+++ lib/reading_time.rb
@@ -12,6 +12,15 @@ class ReadingTime < Middleman::Extension
       minutes_label = minutes === 1 ? ' minute' : ' minutes'
       minutes > 0 ? "about #{minutes} #{minutes_label}" : 'less than 1 minute'
     end
+
+    def time_label(input)
+      words_per_minute = 180
+
+      words = input.split.size
+      minutes = (words/words_per_minute).floor
+      minutes_label = minutes === 1 ? ' minute' : ' minutes'
+      minutes > 0 ? "#{minutes} #{minutes_label}" : 'Under 1 minute'
+    end
   end
 end
 
