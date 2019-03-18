5a23d7b1e597537b73a136e41bdabfc64fe93814
Brett Jones <hello@brettjones.me>
2019-03-18 18:54:10 +0800

    Remove redundant helper

--- config.rb
+++ config.rb
@@ -1,15 +1,3 @@
-module Haml::Filters
-  remove_filter("Markdown") #remove the existing Markdown filter
-
-  module Markdown
-    include Haml::Filters::Base
-
-    def render(text)
-      Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(with_toc_data: true)).render(text)
-    end
-
-  end
-end
 # --------------------------------------------------------------------------------------------------
 # Helpers
 # --------------------------------------------------------------------------------------------------
